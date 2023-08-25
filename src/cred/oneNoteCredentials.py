e5e7890c293e4cf607176c20445f6a28b56d5647
abhinaykhalatkar <abhinaykhalatkar@gmail.com>
2023-08-25 23:46:38 +0200

    final change in credential handling, reload bug

--- src/cred/oneNoteCredentials.py
+++ src/cred/oneNoteCredentials.py
@@ -5,13 +5,11 @@ from threading import Thread
 from credentials import client_id,client_secret,tenant_id
 
 app_running = True 
-# Your app's details
 def stop_flask_app():
     func = request.environ.get('werkzeug.server.shutdown')
     if func:
         func()
     else:
-        # If not running with Werkzeug, use an alternative method
         os._exit(0) 
     
 token_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
@@ -60,16 +58,16 @@ def callback():
     expires_in = token_response_data.get('expires_in', 3600)
     expiry_time = time.time() + expires_in
  
-    if access_token:  # Check if access_token is not None
+    if access_token: 
         with open("src/cred/token.txt", "w") as f:
             f.write(access_token)
         with open('src/cred/token_expiry_time.txt', 'w') as f:
+            print(expiry_time)
             f.write(str(expiry_time))
-            stop_flask_app()
-            app_running = False
+        app_running = False
+        stop_flask_app()
         return "Logged in successfully! You can close this window."
     else:
-        # Handle the case where the access_token is not retrieved
         error_description = token_response_data.get('error_description', 'Unknown error')
         return f"Failed to retrieve access token. Error: {error_description}"
     
@@ -80,16 +78,13 @@ def start_authentication():
     webbrowser.open(auth_url, new=2)
 
 if __name__ == "__main__":
-    # Start the authentication process
     threading.Thread(target=start_authentication).start()
-    
-    # Run the Flask app
+
     t = Thread(target=app.run, kwargs={'debug': False, 'port': 8085, 'use_reloader': False})
     t.start()
-    while app_running:  # Check the status of app_running
-        time.sleep(1)  # Sleep for a short duration to prevent busy-waiting
+    while app_running: 
+        time.sleep(1) 
 
-    t.join()  # Wait for the Flask app thread to finish
-    # app.run(debug=True, port=8085, use_reloader=False)
+    t.join() 
 
 
